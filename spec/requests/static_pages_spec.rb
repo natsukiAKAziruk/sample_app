4219e02c0c6f69edfa0d01a629bf02a0fc1aae0a
natsuki <umukikai000@gmail.com>
2014-09-01 23:39:15 +0900

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,6 +7,11 @@ describe "StaticPages" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
+    it "should have the right title'"  do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    end
   end
 end
 
@@ -17,6 +22,11 @@ describe "Help page" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
+    it "should have the right title'"  do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit '/static_pages/help'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+    end
   end
 end
 
@@ -25,5 +35,9 @@ end
     it "should have the content 'About Us'" do 
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
+    end
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
     end
   end
